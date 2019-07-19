c31ff60d72e00b8f146c2403ab38c6bedc8ed3f4
Archangel603 <mikhailmikhailoff@yandex.ru>
2019-07-19 19:04:18 +0300

    Добавлен prerender plugin

---
+++ vue.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
+module.exports = {
+    filenameHashing: false,
+    productionSourceMap: false,
+
+    configureWebpack: () => {
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                plugins: [
+                    new PrerenderSPAPlugin({
+                        staticDir: path.join(__dirname, 'dist'),
+                        indexPath: path.join(__dirname, 'dist', 'index.html'),
+                        routes: [
+                            "/",
+                            "/pizza",
+                            "/menu",
+                            "/contacts",
+                        ],
+                        minify: {
+                            collapseBooleanAttributes: true,
+                            collapseWhitespace: true,
+                            decodeEntities: true,
+                            keepClosingSlash: true,
+                            sortAttributes: true
+                        },
+
+                        postProcess (renderedRoute) {
+
+                            if (renderedRoute.route !== "/") {
+                                
+                                let parts = renderedRoute.route.split("/");
+                                
+                                renderedRoute.outputPath = path.join(__dirname, 'dist', parts[1] + ".html");
+                            }
+
+                            return renderedRoute
+                        },
+                        
+                        renderer: new Renderer({
+                            injectProperty: '__PRERENDER_INJECTED',
+                            inject: {
+                                isPrerender: true
+                            },
+                            renderAfterDocumentEvent: 'x-app-rendered',
+                        })
+                    })
+                ],
+            };
+        }
+    }
+};
